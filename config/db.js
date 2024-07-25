dd08a7b81eb75f33d7bbdaa4d50f69e89be37d05
tathya09 <106487768+tathya09@users.noreply.github.com>
2024-07-26 00:42:13 +0530

    Final code
    E-commerce fullstack app

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL)
+       
+    }
+    catch (error) {
+        
+    }
+
+}
+
+export default connectDB;
