c7c583712cfe80913536d915c7579e282517339f
tathya09 <106487768+tathya09@users.noreply.github.com>
2024-07-26 12:31:01 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,38 +8,41 @@ import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
-//configure env
+// configure env
 dotenv.config();
 
-//databse config
+// database config
 connectDB();
 
-//rest object
+// rest object
 const app = express();
 
-//middelwares
+// middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(dirname,"./client/build")));
 
-//routes
+// resolve __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+// routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//rest api
-app.use('*',function(req,res)
-{
-  res.sendFile(path.join(dirname,'./client/build/index.html'))
-})
+// rest api
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
-//PORT
+// PORT
 const PORT = process.env.PORT || 8080;
 
-//run listen
+// run listen
 app.listen(PORT, () => {
-
+  console.log(`Server running on port ${PORT}`);
 });
