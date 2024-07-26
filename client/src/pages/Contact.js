4022a9c1ce2d7038023cb339c80fc3545e58f038
tathya09 <106487768+tathya09@users.noreply.github.com>
2024-07-26 14:09:49 +0530

    Update Contact.js

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -15,7 +15,7 @@ const Contact = () => {
         <div className="col-md-4">
           <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
           <p className="text-justify mt-2">
-            any query and info about prodduct feel free to call anytime we 24X7
+            Any query and info about prodduct feel free to call anytime we 24X7
             vaialible
           </p>
           <p className="mt-3">
